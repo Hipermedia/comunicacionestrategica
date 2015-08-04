758a906481e9bcc65d7697155e689e40247c7f7c
cosmo811 <cosmo.op8@gmail.com>
2015-08-04 18:10:47 -0500

    avance sidebar opinion

--- inc/sidebar-opinion.php
+++ inc/sidebar-opinion.php
@@ -4,5 +4,132 @@
 ?>
 <!-- sidebar-opinion -->
 <section class="SidebarOpinion">
-	SidebarOpinion
+	<h2 class="h2">Columnas de opinión</h2>
+	<ul class="nav nav-tabs SidebarOpinion-tabs">
+	  <li class="active">
+	  	<a data-toggle="tab" href="#home">Columna 1</a>
+	  </li>
+	  <li>
+	  	<a data-toggle="tab" href="#menu1">Columna 2</a>
+	  </li>
+	  <li>
+	  	<a data-toggle="tab" href="#menu2">Columna 3</a>
+	  </li>
+	</ul>
+
+	<div class="tab-content">
+	  <div id="home" class="tab-pane fade in active">
+		<div class="SidebarOpinion-ficha">
+			<figure>
+				<i class="fa fa-user"></i>
+			</figure>
+			<h2>Nombre del artículo de la columna</h2>
+			<p>Nombre del columnista</p>
+		</div>
+		<div class="SidebarOpinion-ficha">
+			<figure>
+				<i class="fa fa-user"></i>
+			</figure>
+			<h2>Nombre del artículo de la columna</h2>
+			<p>Nombre del columnista</p>
+		</div>
+		<div class="SidebarOpinion-ficha">
+			<figure>
+				<i class="fa fa-user"></i>
+			</figure>
+			<h2>Nombre del artículo de la columna</h2>
+			<p>Nombre del columnista</p>
+		</div>
+		<div class="SidebarOpinion-ficha">
+			<figure>
+				<i class="fa fa-user"></i>
+			</figure>
+			<h2>Nombre del artículo de la columna</h2>
+			<p>Nombre del columnista</p>
+		</div>
+		<div class="SidebarOpinion-ficha">
+			<figure>
+				<i class="fa fa-user"></i>
+			</figure>
+			<h2>Nombre del artículo de la columna</h2>
+			<p>Nombre del columnista</p>
+		</div>
+	  </div>
+
+	  <div id="menu1" class="tab-pane fade">
+		<div class="SidebarOpinion-ficha">
+			<figure>
+				<i class="fa fa-user"></i>
+			</figure>
+			<h2>Nombre del artículo de la columna</h2>
+			<p>Nombre del columnista</p>
+		</div>
+		<div class="SidebarOpinion-ficha">
+			<figure>
+				<i class="fa fa-user"></i>
+			</figure>
+			<h2>Nombre del artículo de la columna</h2>
+			<p>Nombre del columnista</p>
+		</div>
+		<div class="SidebarOpinion-ficha">
+			<figure>
+				<i class="fa fa-user"></i>
+			</figure>
+			<h2>Nombre del artículo de la columna</h2>
+			<p>Nombre del columnista</p>
+		</div>
+		<div class="SidebarOpinion-ficha">
+			<figure>
+				<i class="fa fa-user"></i>
+			</figure>
+			<h2>Nombre del artículo de la columna</h2>
+			<p>Nombre del columnista</p>
+		</div>
+		<div class="SidebarOpinion-ficha">
+			<figure>
+				<i class="fa fa-user"></i>
+			</figure>
+			<h2>Nombre del artículo de la columna</h2>
+			<p>Nombre del columnista</p>
+		</div>
+	  </div>
+
+	  <div id="menu2" class="tab-pane fade">
+		<div class="SidebarOpinion-ficha">
+			<figure>
+				<i class="fa fa-user"></i>
+			</figure>
+			<h2>Nombre del artículo de la columna</h2>
+			<p>Nombre del columnista</p>
+		</div>
+		<div class="SidebarOpinion-ficha">
+			<figure>
+				<i class="fa fa-user"></i>
+			</figure>
+			<h2>Nombre del artículo de la columna</h2>
+			<p>Nombre del columnista</p>
+		</div>
+		<div class="SidebarOpinion-ficha">
+			<figure>
+				<i class="fa fa-user"></i>
+			</figure>
+			<h2>Nombre del artículo de la columna</h2>
+			<p>Nombre del columnista</p>
+		</div>
+		<div class="SidebarOpinion-ficha">
+			<figure>
+				<i class="fa fa-user"></i>
+			</figure>
+			<h2>Nombre del artículo de la columna</h2>
+			<p>Nombre del columnista</p>
+		</div>
+		<div class="SidebarOpinion-ficha">
+			<figure>
+				<i class="fa fa-user"></i>
+			</figure>
+			<h2>Nombre del artículo de la columna</h2>
+			<p>Nombre del columnista</p>
+		</div>
+	  </div>
+	</div>
 </section>
