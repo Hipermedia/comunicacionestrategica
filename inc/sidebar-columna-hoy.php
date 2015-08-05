feb56a3a13e8c0a3b45734b62f9fdaa3ba611613
cosmo811 <cosmo.op8@gmail.com>
2015-08-04 19:06:03 -0500

    Bloque de columna hoy con ventana modal

--- inc/sidebar-columna-hoy.php
+++ inc/sidebar-columna-hoy.php
@@ -3,17 +3,34 @@
 ------------------------------------------------------------------- */ 
 ?>
 <section class="SidebarColumnaHoy">
-	<p>Columna Hoy</p>
+	<h2 class="h2">Columna Hoy</h2>
 	<figure>
-		<img src="<?php echo get_plantilla_url()."/images/foto-columnista.jpg"; ?>" alt="">
+		<i class="fa fa-user"></i>
+		<!-- <img src="" alt=""> -->
 	</figure>
-	<h2>
+	<h2 class="SidebarColumnaHoy-titulo">
 		Título del artículo de la columna
 	</h2>
-	<h3>
+	<h3 class="SidebarColumnaHoy-columnista">
 		Nombre del columnista
 	</h3>
-	<div class="SidebarColumnaHoy-contenido">
-		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim vel, accusantium sapiente architecto qui doloribus suscipit asperiores itaque, deleniti. Corrupti dolorum consequatur, repudiandae debitis explicabo voluptas earum blanditiis placeat, possimus eaque expedita dolorem facilis eius provident quia dolores, aspernatur sequi a est et nam alias ducimus doloribus aliquam. Odio qui distinctio accusantium ipsum repellendus? Cum sint odio vero, distinctio perspiciatis error provident voluptatum earum, aliquam? Iusto, magnam, ratione? Odio inventore, aliquam! Explicabo, consectetur. Facere deserunt, doloribus atque hic quo doloremque voluptatum ut, ducimus architecto consequuntur debitis minima minus odio ea error et dolorem laudantium. Perspiciatis excepturi molestias, unde, aliquid dolorum architecto dignissimos eaque sequi hic accusantium autem tempore, officiis fugit doloribus deserunt? Error eius accusantium qui aperiam vitae, amet at.
-	</div>
+	<button type="button" class="btn btn-default" data-toggle="modal" data-target="#myModal">Leer</button>
 </section>
+
+<!-- Modal -->
+<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h4 class="modal-title" id="myModalLabel">Título de la columna</h4>
+      </div>
+      <div class="modal-body">
+      	Lorem ipsum dolor sit amet, consectetur adipisicing elit. Libero, veritatis unde impedit quibusdam, consequuntur, soluta doloremque, doloribus recusandae animi beatae facilis perspiciatis architecto esse sit! Alias voluptatum sed inventore eligendi tenetur similique nesciunt accusamus natus amet temporibus nihil, totam libero dolor aliquid neque sit vero voluptates fugiat? Delectus explicabo dicta, aspernatur repellendus eveniet eos. Culpa nulla architecto reprehenderit debitis optio praesentium magni quibusdam cupiditate. Maiores non suscipit sit esse fugiat qui et officia ad saepe, nisi facilis dolorum at, ab repellat eum nulla nobis. Voluptatem ratione quam vero, itaque debitis eum consequuntur, aut, numquam sunt fuga perspiciatis. Soluta, ratione, optio. Sed saepe perspiciatis odit libero doloremque quibusdam placeat, deserunt architecto laborum quaerat consequuntur ipsa temporibus officiis tempore facere, error cupiditate iusto iure sit possimus officia inventore eos? Cum a sint voluptates, ipsa, assumenda tenetur deserunt fugit impedit quidem odio commodi veritatis et, labore suscipit reiciendis expedita voluptatibus neque consectetur! Iure omnis et eos cum nisi, officia voluptatem saepe ut assumenda, necessitatibus. Ex expedita autem cumque voluptatem deserunt ut repellendus, itaque fugit officia! Est mollitia fugit, nemo voluptate quo odio doloremque voluptates, esse quia, hic voluptatum aliquam ipsam quaerat accusantium adipisci earum repellat vero rem quae perferendis unde odit ipsum natus?
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>
+      </div>
+    </div>
+  </div>
+</div>
